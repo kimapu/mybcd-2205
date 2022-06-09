82a8fe27e7f728d99ae5b37a7585a9d550ce34fe
kimlee <kimlee@10.111.65.86>
2022-06-09 15:02:24 +0800

    bcd-push/exercise/block

---
+++ mybcd-2205/src/bcd/exercise/dummytranx/MyDummyBlockchain.java
@@ -0,0 +1,12 @@
+package bcd.exercise.dummytranx;
+
+public class MyDummyBlockchain {
+
+	/**
+	 * EXERCISE: Given my.bcd.exercise.dummytranx.dummytranx.txt consisting 23 transactions. 
+	 * 	
+	 * 	Create the Blocks to keep all transactions.
+	 */
+
+	
+}
